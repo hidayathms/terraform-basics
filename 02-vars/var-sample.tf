7043e4abd250327f8371e840c7048be7b0eb189a
Hidayath <hidayath@aol.com>
2024-06-04 11:43:05 +0530

    Terraform Update

--- 02-vars/var-sample.tf
+++ 02-vars/var-sample.tf
@@ -9,5 +9,11 @@ variable "batch" {
 # printing a variable
 output "batch-out" {
     value = var.batch
-  
 }
+
+# when to user varialbes in quotes and $ notations
+# Whenever your variable is enclosed in a set of string or between a sentance, then we have to enclose them in strings and with bash notation.data 
+
+output " batch_number {
+value = "(Current bactch is var.batch)"
+}"
