ee5390c84779691ca9738be026a4c242b46a0b77
Hidayath <hidayath@aol.com>
2024-06-05 16:41:30 +0530

    Terraform Update

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow SSH inbound traffic and outbound traffic"
 
     ingress {
-    cidr_blocks =     = ["0.0.0.0/0"]
+    cidr_blocks       = ["0.0.0.0/0"]
     from_port         = 22
     protocol          = "tcp"
     to_port           = 22
