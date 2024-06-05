adff6909a17499a95fcb0820aca7548203755c56
Hidayath <hidayath@aol.com>
2024-06-05 20:00:36 +0530

    Terraform Update

--- 04-datasource/ec2.tf
+++ 04-datasource/ec2.tf
@@ -1,3 +1,5 @@
+terraform import aws_security_group.b56_allow_tls sg-0f32dfce793fa31bc
+
 resource "aws_instance" "web" {
     # ami                          = "ami-0f75a13ad2e340a58"
   ami                          = data.aws_ami.ami.id
