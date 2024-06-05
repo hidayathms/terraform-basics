cff6813f4b72ff2e9597a41d2fa0aa0ffc479439
Hidayath <hidayath@aol.com>
2024-06-05 14:09:19 +0530

    Terraform Update

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -35,9 +35,9 @@ resource "aws_vpc_security_group_ingress_rule" "SSH from vpc" {
 }
 
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_egress_rule" "vpc to SSH" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = ["0.0.0.0/0"]
   ip_protocol       = "-1" 
 }
 
