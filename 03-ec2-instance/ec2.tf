f6cfa1afdb1d8fc669ae8cfa5447ba07cb7f6603
Hidayath <hidayath@aol.com>
2024-06-05 16:44:15 +0530

    Terraform Update

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -28,8 +28,8 @@ resource "aws_security_group" "allow_tls" {
     to_port           = 22
     }
     egress {
-    from_port         = 22
-    to_port           = 22
+    from_port         = 0
+    to_port           = 0
     cidr_blocks       = ["0.0.0.0/0"]
     protocol          = "ALL" 
 }
