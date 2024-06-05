974deae40a494dcce8b32d1a52e44ca6e78a59f1
Hidayath <hidayath@aol.com>
2024-06-05 19:38:38 +0530

    Terraform Update

--- 04-datasource/ec2.tf
+++ 04-datasource/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
     # ami                          = "ami-0f75a13ad2e340a58"
-  ami                          = data.aws_ami.ami.id
+  ami                          = [data.aws_ami.ami.id]
   instance_type                = "t2.micro"
   vpc_security_group_ids       = [aws_security_group.allow_tls.id]
 
