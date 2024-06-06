37d66d3069687c03155b41cb6cf4dca86b4f253c
Hidayath <hidayath@aol.com>
2024-06-06 12:18:35 +0530

    Terraform Update

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -14,3 +14,9 @@ terraform {
 module "local-module" {
     source = "./local"
 }
+
+# This is how you fectch the values from a backend moduel
+# module.moduleName.outputNameDeclaredInTheBackend
+output "public_ip"{
+    value =module.local-module.public_ip
+}
