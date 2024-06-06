3e46824ba3f17c3377b47a2636f5b9048497332f
Hidayath <hidayath@aol.com>
2024-06-06 12:02:48 +0530

    Terraform Update

--- 05-modules/local/ec2.tf
+++ 05-modules/local/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
     # ami                          = "ami-0f75a13ad2e340a58"
-  ami                          = data.aws_ami.ami.id
+  ami                          = ami-0f75a13ad2e340a58
   instance_type                = "t2.micro"
   
 }
