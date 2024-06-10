d5d14a910b1fcc26237c648915872a9bda4da31f
Hidayath <hidayath@aol.com>
2024-06-10 13:52:03 +0530

    Terraform Update

--- 06-modules/ec2/remoteProvisioner.tf
+++ 06-modules/ec2/remoteProvisioner.tf
@@ -12,12 +12,12 @@ resource "null_resource" "playbook" {
 
     provisioner "remote-exec" {
         inline = [
-          "ansible-pull -i inv -U https://github.com/hidayathms/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml ",
+          "ansible-pull-U https://github.com/hidayathms/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml ",
         ] 
     }
 }
 
-
+ 
 # 1. Creates Resource
 # 2. Null provisioner authenticates/establishes connection to the newly created resource
 # 3. Then execute tasks mentioned in the remote exec
