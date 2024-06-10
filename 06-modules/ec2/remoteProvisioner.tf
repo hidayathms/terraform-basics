095145900f6761ceac7d342cd1e8024db587cbbc
Hidayath <hidayath@aol.com>
2024-06-10 14:11:25 +0530

    Terraform Update

--- 06-modules/ec2/remoteProvisioner.tf
+++ 06-modules/ec2/remoteProvisioner.tf
@@ -1,19 +1,18 @@
-# #Demo on remote Provisioner
-
 resource "null_resource" "playbook" {
-    # This depends on make sures that the null resource will only be executed post the creation of the aws_instance.app
+
+    # This make sures that this null_resource will only be executed post the creation of the aws_instance.app only    
     depends_on = [aws_instance.app]
-    connection {    
-        type     = "ssh"    
-        user     = "centos"    
-        password = "DevOps321"    
-        host     = aws_instance.app.private_ip  
+    connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = aws_instance.app.private_ip
     }
 
     provisioner "remote-exec" {
         inline = [
-          "ansible-pull -U https://github.com/hidayathms/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml ",
-        ] 
+            "ansible-pull -U https://github.com/b56-clouddevops/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml"
+        ]
     }
 }
 
