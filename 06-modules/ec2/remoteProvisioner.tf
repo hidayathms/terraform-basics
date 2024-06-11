2bf244bb7c4e35cee890476ca721590aa714f020
Hidayath <hidayath@aol.com>
2024-06-11 09:18:06 +0530

    Terraform Update

--- 06-modules/ec2/remoteProvisioner.tf
+++ 06-modules/ec2/remoteProvisioner.tf
@@ -12,7 +12,7 @@ resource "null_resource" "playbook" {
     provisioner "remote-exec" {
         inline = [
             "git clone https://github.com/hidayathms/ansible.git",
-            "ansible-pull -U https://github.com/hidayathms/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml"
+            "ansible-pull -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml"
         ]
     }
 }
